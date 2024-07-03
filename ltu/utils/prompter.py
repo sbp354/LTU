e0d89625a7c9ee0f323be21bf00f528c71883940
Sara Price <sbp354@nyu.edu>
2024-07-03 04:06:55 +0000

    updating dependencies and prompter file path

--- ltu/utils/prompter.py
+++ ltu/utils/prompter.py
@@ -17,7 +17,7 @@ class Prompter(object):
             template_name = "alpaca"
         file_name = osp.join("../templates", f"{template_name}.json")
         if not osp.exists(file_name):
-            file_name = osp.join("./templates", f"{template_name}.json")
+            file_name = osp.join("../templates", f"{template_name}.json")
         if not osp.exists(file_name):
             raise ValueError(f"Can't read {file_name}")
         with open(file_name) as fp:
